6467992ebf3ff1f2486fa976c1198bc0f3c5cb46
Shamyrat Gutlygeldiyev <gorkakdal@gmail.com>
2022-10-30 02:30:57 +0100

    initial

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Home from "./pages/Home";
+// import ProductList from "./pages/ProductList";
+// import Product from "./pages/Product";
+// import Cart from "./pages/Cart";
+// import Navbar from "./components/Navbar";
+// import Login from "./pages/Login";
+// import Register from "./pages/Register";
+
+const App = () => {
+  return <Home />
+};
+
+export default App;
